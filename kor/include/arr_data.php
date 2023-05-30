dd06c0a0e583b7e4c2d87cdf9da07f4dbaaeb427
furaha707 <zxcvyein950823@gmail.com>
2023-05-30 13:48:30 +0900

    Initial commit

---
+++ kor/include/arr_data.php
@@ -0,0 +1,80 @@
+<?
+
+$site_title = "예림임업"; // 사이트명
+
+$P_lang = "ko"; // html language
+
+$P_company_name = "(주)예림임업"; // 회사명
+
+$P_addr1 = "인천광역시 남동구 논현고잔로 135번길 29(고잔동)"; // 주소1
+$P_addr2 = ""; // 주소2
+
+$P_tel1 = "";
+$P_tel2 = "";
+
+$P_fax1 = "";
+$P_fax2 = "";
+
+$P_email1 = "";
+$P_email2 = "";
+
+$P_business_no = "137-81-69052"; // 사업자등록번호
+
+$P_sales_no = ""; // 통신판매업신고번호
+
+$P_ceo = "전용범"; // 대표자
+
+// 네비게이션 메뉴명
+$nav_1 = "기업소개";
+$nav_1_1 = "CEO 인사말";
+$nav_1_2 = "비젼 및 경영전략";
+$nav_1_3 = "연혁";
+$nav_1_4 = "CI소개";
+$nav_1_5 = "찾아오시는길";
+
+$nav_2 = "사업영역";
+$nav_2_1 = "예림도어";
+$nav_2_2 = "예림인테리어키친";
+$nav_2_3 = "예림자연창샤시";
+
+$nav_3 = "인재채용";
+$nav_3_1 = "인재상";
+$nav_3_2 = "채용프로세스";
+$nav_3_3 = "FAQ";
+
+$nav_policy = "홈페이지 정책";
+$nav_policy_1 = "개인정보처리방침";
+$nav_policy_2 = "이용약관";
+$nav_policy_3 = "이메일무단수집거부";
+
+// 네비게이션 하이퍼링크
+$nav_1_href = "../company/ceo.html";
+$nav_1_1_href = "../company/ceo.html";
+$nav_1_2_href = "../company/vision.html";
+$nav_1_3_href = "../company/history.html";
+$nav_1_4_href = "../company/ci.html";
+$nav_1_5_href = "../company/location.html";
+
+$nav_2_href = "../business/door.html";
+$nav_2_1_href = "../business/door.html";
+$nav_2_2_href = "../business/kitchen.html";
+$nav_2_3_href = "../business/window.html";
+
+$nav_3_href = "../recruit/recruit.html";
+$nav_3_1_href = "../recruit/recruit.html";
+$nav_3_2_href = "../recruit/process.html";
+$nav_3_3_href = "../recruit/faq.html";
+
+$nav_policy_href = "../policy/privacy.html";
+$nav_policy_1_href = "../policy/privacy.html";
+$nav_policy_2_href = "../policy/term.html";
+$nav_policy_3_href = "../policy/email-security.html";
+
+// 서브비주얼
+$sv_1_name = "";
+$sv_2_name = "";
+$sv_3_name = "";
+$sv_4_name = "";
+$sv_5_name = "";
+
+?>
